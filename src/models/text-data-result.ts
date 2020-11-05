f11446d139c1f3ac59687a5d46974a0c90ff7250
GitHub Action <action@github.com>
2020-11-05 16:19:48 +0000

    5.3.0.beta1

--- src/models/text-data-result.ts
+++ src/models/text-data-result.ts
@@ -22,6 +22,7 @@ import { ImagesResult } from './images-result';
 import { LexicalAnalysisResult } from './lexical-analysis-result';
 import { ResultItem } from './result-item';
 import { StatusResult } from './status-result';
+import { TextDataResult } from './text-data-result';
 import { TextDataResultAllOf } from './text-data-result-all-of';
 import { TextResult } from './text-result';
 
