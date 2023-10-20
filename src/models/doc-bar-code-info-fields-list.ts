65e380500e0a067083056821406dcea69c1186fc
GitHub Action <action@github.com>
2023-10-20 11:01:59 +0000

    test-front

--- src/models/doc-bar-code-info-fields-list.ts
+++ src/models/doc-bar-code-info-fields-list.ts
@@ -1,7 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
- * Regula Document Reader Web API
+ * Regula Document Reader Web
  * Documents recognition as easy as reading two bytes.  # Clients: * [JavaScript](https://github.com/regulaforensics/DocumentReader-web-js-client) client for the browser and node.js based on axios * [Java](https://github.com/regulaforensics/DocumentReader-web-java-client) client compatible with jvm and android * [Python](https://github.com/regulaforensics/DocumentReader-web-python-client) 3.5+ client * [C#](https://github.com/regulaforensics/DocumentReader-web-csharp-client) client for .NET & .NET Core 
  *
  * The version of the OpenAPI document: 6.9.0
