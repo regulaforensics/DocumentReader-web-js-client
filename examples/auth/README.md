ac5d561babcd0ea3d096f0e2f3a8d226a1c3fa4d
Kiryl Kovaliov <kkirusha@gmail.com>
2023-11-23 10:22:47 +0100

    update

---
+++ examples/auth/README.md
@@ -0,0 +1,5 @@
+TDB
+
+- description how it works
+- how to start server
+- how to run the client
--- examples/auth/server/index.js
+++ examples/auth/server/index.js
@@ -3,71 +3,9 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import axios from "axios";
-import jwt from "jsonwebtoken";
-import jwkToPem from "jwk-to-pem";
-import { prop, path, find, compose, flip, curryN } from "ramda";
+import { verifyOffline } from 'auth';
 
 const app = express();
-
-const verify = curryN(2)(jwt.verify);
-const IDP_KEYCLOAK_URL = "http://keycloak:8080"
-
-const IDP_KEYCLOAK_REALM = "regula"
-
-
-const findPublicKeyFromKid = publicKey => kid =>
-  find(isTheRightKid(kid))(publicKey);
-
-const getKid = path(["header", "kid"]);
-
-const decode = compose(
-  curryN(2),
-  flip
-)(jwt.decode);
-
-const getUserFromPublicKey = token =>
-  compose(
-    makeUser,
-    verify(token)
-  );
-
-  const makeUser = ({
-    sub,
-    preferred_username,
-    email_verified,
-    resource_access,
-    email,
-    name,
-    ...others
-  }) => ({
-    id: sub,
-    userName: preferred_username,
-    emailVerified: email_verified,
-    resourceAccess: resource_access,
-    email,
-    name,
-    ...others
-  });
-
-const isTheRightKid = kid => publicKey => publicKey.kid === kid;
-
-const getUserFromJWK = token => jwk =>
-  compose(
-    getUserFromPublicKey(token),
-    jwkToPem,
-    findPublicKeyFromKid(jwk),
-    getKid,
-    decode({ complete: true })
-  )(token);
-
-const verifyOffline = (accessToken, options = {}) => {
-  const url = `${IDP_KEYCLOAK_URL}/realms/${IDP_KEYCLOAK_REALM}/protocol/openid-connect/certs`;
-
-  return axios.get(url)
-    .then(path(["data", "keys"]))
-    .then(getUserFromJWK(accessToken))
-};
 
 
 app.use(helmet());
@@ -80,16 +18,25 @@ app.get('/ping', async (req, res) => {
 });
 
 app.get('/verify-token', async (req, res) => {
+
+  // Authorization header not set
   if (!(req.header("Authorization") && req.header("Authorization").startsWith('Bearer '))) {
     return res.status(401).json({message: "Invalid Authorization"});
   }
+
   try
   {
     console.log(req.header("Authorization"))
+
+    // Verify authorization token
     await verifyOffline(req.header("Authorization").slice(7))
+
+    // authorization secceeded
     return res.status(200).json({});
   }
   catch (e){
+
+    // authorization failed
     console.error(`Error: ${e.message}`);
     return res.status(401).json({message: "Unauthorized" });
   }
