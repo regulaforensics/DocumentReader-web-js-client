b94f2e6ba7753c8adf2c78db6eaf4d703741e1fd
Artem Tsybulko <tsybulko.artem@gmail.com>
2025-03-24 15:30:08 +0300

    SP-21988 - add health()

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -19,7 +19,7 @@ const api = new DocumentReaderApi({ basePath: apiBasePath });
 // Uncomment the line below if you want to transfer the license with each request
 // api.setLicense(license);
 
-const serverInfo = await api.ping();
+const serverInfo = await api.health();
 
 const white_page_0 = fs.readFileSync('WHITE.jpg').buffer;
 
