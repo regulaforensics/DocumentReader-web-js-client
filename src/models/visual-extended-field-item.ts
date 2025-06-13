68add864e23f3c5eebeaea79ae4cc8db7e0edab4
Artem Tsybulko <tsybulko.artem@gmail.com>
2025-06-13 13:00:45 +0300

    SP-18211 - update models

--- src/models/visual-extended-field-item.ts
+++ src/models/visual-extended-field-item.ts
@@ -15,6 +15,9 @@
 
 // May contain unused imports in some cases
 // @ts-ignore
+import type { LCID } from './lcid';
+// May contain unused imports in some cases
+// @ts-ignore
 import type { StringRecognitionResult } from './string-recognition-result';
 // May contain unused imports in some cases
 // @ts-ignore
@@ -82,10 +85,10 @@ export interface VisualExtendedFieldItem {
     'InComparison'?: number;
     /**
      * 
-     * @type {number}
+     * @type {LCID}
      * @memberof VisualExtendedFieldItem
      */
-    'wLCID'?: number;
+    'wLCID'?: LCID;
     /**
      * 
      * @type {number}
