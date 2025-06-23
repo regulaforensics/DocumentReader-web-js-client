a9c93934b9d658ec7d42edb4f9d6ed8bfb1eeb27
Artem Tsybulko <tsybulko.artem@gmail.com>
2025-06-23 17:49:01 +0300

    SP-24000 - parcel -> vite

--- eslint.config.js
+++ eslint.config.js
@@ -4,21 +4,15 @@ import tseslint from 'typescript-eslint';
 import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
-    {
-        files: ['**/*.{js,mjs,cjs,ts}'],
-    },
-    {
-        languageOptions: {
-            globals: { ...globals.browser, ...globals.node },
-        },
-    },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     prettierRecommended,
     {
+        files: ['**/*.{js,mjs,cjs,ts}'],
+        languageOptions: {
+            globals: { ...globals.browser, ...globals.node },
+        },
         ignores: ['dist/*', 'lib/*', '!.*'],
-    },
-    {
         rules: {
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/no-unused-vars': 'off',
